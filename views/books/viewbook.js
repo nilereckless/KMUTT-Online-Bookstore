b273c01cc8aac7f357cedc06d5db3c20e50cd6cc
kongtapxxx <kongtap.ikunearth@mail.kmutt.ac.th>
2021-04-19 17:07:10 +0700

    add react module

---
+++ views/books/viewbook.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class viewbook extends Component {
+    state = { 
+
+     }
+     
+    render() { 
+        return ( 
+            console.log('im so happy to coding')
+         );
+    }
+}
+ 
+export default viewbook;
