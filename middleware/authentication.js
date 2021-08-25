6e33454388e567f0cd5f38ab432668eda302cc1b
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-08-25 12:07:16 +0700

    google

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,6 +3,6 @@ let staff = require('../controller/staff');
 exports.checkAdmin = async (req, res, next) => {
     //var staffInfo = await staff.getStaff()
     //console.log(staffInfo);
-    console.log(req.user);
+    console.log(req.user?.emails[0].value);
     next()
 }
