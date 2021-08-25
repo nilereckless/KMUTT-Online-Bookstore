b0e57743051048f797e9aa9ac6fd7c4a9dc7fd41
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-08-25 12:04:36 +0700

    google

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,8 +1,8 @@
 let staff = require('../controller/staff');
 
 exports.checkAdmin = async (req, res, next) => {
-    var staff = await staff.getStaff()
-    console.log(staff);
+    var staffInfo = await staff.getStaff()
+    console.log(staffInfo);
     console.log(req.user.emails[0].value);
     next()
 }
