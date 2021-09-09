315b4b35edf6d831cf1040fe0eabb540e1456015
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-09 21:01:20 +0700

    authen cart

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use('/cart', cartRouter);
 passport.use('google-authenticate', new CustomStrategy(
     function(req, done) {
     verify(req.body.id_token).then((e) => {
-      done(err, e);
+      done(null, e);
     })
   }
 ));
