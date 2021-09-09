076a295e46c4f23ebc9e1cb8ada64f42ca34a980
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-09 13:55:18 +0700

    authen cart

--- routes/cart.js
+++ routes/cart.js
@@ -8,7 +8,7 @@ const middleWare = require('../middleware/authentication') ;
 
 
 //middleware.isAuthenticated(), วางไว้หน้า async
-router.get('/', async (req, res, next) => {
+router.get('/', middleware.isAuthenticated, async (req, res, next) => {
     var cart = null;
     //   console.log(cartStorage[1]) ;
     if (cartStorage[req.user.id] === undefined) {
