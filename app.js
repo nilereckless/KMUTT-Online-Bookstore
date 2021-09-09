e00dab4a412cbb0ae169547761e4f88f1c642149
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-09 21:04:35 +0700

    authen cart

--- app.js
+++ app.js
@@ -53,6 +53,13 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 
 app.use(flash());
