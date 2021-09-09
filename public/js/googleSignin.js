be81912a8a3ec66429b420a54c7aec0ddc359d1a
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-09 15:31:03 +0700

    authen cart

--- public/js/googleSignin.js
+++ public/js/googleSignin.js
@@ -3,7 +3,7 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     var url = '/auth/google/callback';
        var request = $.post(url, {
-        data: id_token,
+        id_token: id_token,
        }) ;
         request.done(function(e) {
           console.log(e);
