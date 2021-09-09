c9be268664ae7e9a69b6ad5a8bee894b599a9913
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-09 14:19:56 +0700

    authen cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -6,6 +6,7 @@ $( document ).ready(function() {
        var request = $.get(url) ;
         request.done(function(e) {
           if(e === "Unauthorized"){
+            $('#loginmodal').modal('toggle')
           //  window.location.href = '/login?next=cart' ; //หน้าLogin แล้วเสร็จปุ๊บค่อยไปหน้าCart
           } else {
           if($("#cartCount").attr("data-count") > 0){
