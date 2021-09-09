da47d8a9a7f692b94237824b168902bb3d1db7f0
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-09 16:47:07 +0700

    authen cart

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ passport.use('strategy-name', new CustomStrategy(
 passport.use(new CustomStrategy(
     function(req, done) {
     verify(req.body.id_token).then((e)=> {
-      console.log(e)
+      done(null, e)
     })
     // done(null, user);
   }
