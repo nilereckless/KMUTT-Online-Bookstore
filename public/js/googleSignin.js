47d9eb43b98ae5f02cadf5b10753d33d63dcfab7
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-10 22:11:55 +0700

    testday2

--- public/js/googleSignin.js
+++ public/js/googleSignin.js
@@ -1,18 +1,6 @@
 
 function onSignIn(googleUser) {
-  var id_token = googleUser.getAuthResponse().id_token;
-  var url = '/auth/google/callback';
-  var request = $.post(url, {
-    id_token: id_token,
-  });
-  request.done(function (e) {
-    console.log(e);
-    alert("loginSuccess");
-  })
-  request.fail(function (e) {
-    console.log(e)
-    alert("error");
-  })
+  console.log(googleUser);
 }
 
 function login() {
