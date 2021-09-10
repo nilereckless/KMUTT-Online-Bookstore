9ebc296ee0b21450a9bc024afdc9d24c9f742770
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-10 21:59:33 +0700

    testday2

--- public/js/googleSignin.js
+++ public/js/googleSignin.js
@@ -1,16 +1,33 @@
 
 function onSignIn(googleUser) {
-    var id_token = googleUser.getAuthResponse().id_token;
-    var url = '/auth/google/callback';
-       var request = $.post(url, {
-        id_token: id_token,
-       }) ;
-        request.done(function(e) {
-          console.log(e);
-          alert("loginSuccess");
-        })
-        request.fail(function(e) {
-            console.log(e)
-          alert( "error" );
-        })
-  }
+  var id_token = googleUser.getAuthResponse().id_token;
+  var url = '/auth/google/callback';
+  var request = $.post(url, {
+    id_token: id_token,
+  });
+  request.done(function (e) {
+    console.log(e);
+    alert("loginSuccess");
+  })
+  request.fail(function (e) {
+    console.log(e)
+    alert("error");
+  })
+}
+
+function login() {
+  var myParams = {
+    'clientid': '315716910345-28jpa507rrqnitgj7a5jd2dolrdqcpun.apps.googleusercontent.com',
+    'cookiepolicy': 'single_host_origin',
+    'callback': 'loginCallback',
+    'approvalprompt': 'force',
+    'scope': 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+  };
+
+  gapi.auth.signIn(myParams);
+}
+
+ $("#googlelogin").on("click", function (e) {
+   e.preventDefault();
+   login();
+ })
