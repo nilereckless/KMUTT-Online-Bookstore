b6100f8b684619a8c64c941d286eb74ccd85b802
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-10 22:47:33 +0700

    testday2

--- public/js/googleSignin.js
+++ public/js/googleSignin.js
@@ -16,18 +16,33 @@ function onSignIn(googleUser) {
   })
 }
 
-function login() {
-  var myParams = {
-    clientid: '315716910345-28jpa507rrqnitgj7a5jd2dolrdqcpun.apps.googleusercontent.com',
-    cookiepolicy: 'single_host_origin',
-    callback: 'onSignIn',
-    approvalprompt: 'force'
+
+//  $("#googlelogin").on("click", function (e) {
+//    e.preventDefault();
+//    login();
+//  })
+
+  var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '315716910345-28jpa507rrqnitgj7a5jd2dolrdqcpun.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        scope: 'profile email'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
   };
 
-  gapi.auth.signIn(myParams);
-}
-
- $("#googlelogin").on("click", function (e) {
-   e.preventDefault();
-   login();
- })
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+          console.log(googleUser)
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
+  
