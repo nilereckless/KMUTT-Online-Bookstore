a42fce24c9d124a3f4c66b1f5bb75ff71719f374
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-11 17:17:02 +0700

    testday3

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -28,6 +28,7 @@ exports.isStaffAuthenticated = async (req, res, next) => {
     } else {
         var staffInfo = await staff.getStaff(req.user.email)
         if (staffInfo.length === 0) {
+            req.flash('error', 'Only staff login first!');
             res.redirect('/');
         }
     }
