14926ba170711f66fc0a6ed6ef5955096a27ec53
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-11 16:19:11 +0700

    testday3

--- public/js/googleSignin.js
+++ public/js/googleSignin.js
@@ -32,7 +32,7 @@ function onSignIn(googleUser) {
         // Request scopes in addition to 'profile' and 'email'
         scope: 'profile email'
       });
-      attachSignin(document.getElementsByClassName('googlesignin'));
+      attachSignin(document.querySelector('[id^="google-"]').id);
     });
   };
 
