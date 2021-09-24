efdd385c4fb3293fa5a015f47122bf8434b0d3fe
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-09-24 16:01:36 +0700

    test1

--- public/js/googleSignin.js
+++ public/js/googleSignin.js
@@ -2,7 +2,7 @@
 function onSignIn(googleUser) {
   console.log(googleUser);
   var id_token = googleUser.Zb.id_token;
-  console.log("prince", googleUser.Zb);
+  console.log("prince");
   var url = '/auth/google/callback';
   var request = $.post(url, {
     id_token: id_token,
