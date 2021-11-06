0d9b113dae0b1f229fbb95575a450ffb814ffcf7
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-06 15:50:34 +0700

    test1

--- controller/bookController.js
+++ controller/bookController.js
@@ -14,7 +14,7 @@ exports.getBookByID = (bookID) => {
 
 exports.updateBookStockByID = (bookID, amount) => {
     return new Promise((resolve, reject) => {
-        var query = `UPDATE books SET stock=stock-${amount} FROM books WHERE id = '${bookID}'`;
+        var query = `UPDATE books SET stock = stock-${amount} WHERE id= ${bookID}`;
         dbConn.query(query, (err, rows) => {
             if (err) {
                 reject(err);
