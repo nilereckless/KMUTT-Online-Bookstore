b565569e9df1743bc2831d0fb346568e5e5cd1c9
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-08 14:50:39 +0700

    test

--- routes/cart.js
+++ routes/cart.js
@@ -160,6 +160,7 @@ router.post('/checkout', middleWare.isAuthenticatedCart, async (req, res, next) 
     var orderID = Math.round(Math.floor(Date.now() / 1000))
     //written by arit
     var shipID = req.query.shipIDtoSend;
+    console.log(shipID);
     var address = await shipController.getShippingAddressByShipID(req.body.address);
     var cart = null;
     var shipData = await shipController.getShippingAddressByShipID(shipID);
