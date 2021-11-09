efdab78a9de327b3f7bdd0e2deda02b47930298c
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-09 16:12:38 +0700

    test

--- controller/orderHistoryController.js
+++ controller/orderHistoryController.js
@@ -26,7 +26,7 @@ exports.addOrderHistoryByID = (user_id, order_id, payment_option, shipaddress_id
 
 exports.addAllOrderByID = (user_id, order_id, book_name, quantity, total_price, book_id, shipID, district, province, postalcode, address, subdistrict) => {
     return new Promise((resolve, reject) => {
-        var query = `INSERT INTO order_books (user_id, order_id, book_name, quantity, total_price, book_id, shipID, district, province, postalCode, address, subdistrict) VALUES ( '${user_id}', '${order_id}', '${book_name}', '${quantity}','${total_price}','${book_id}', '${shipID}', '${district}', '${province}', '${postalcode}', '${address}', '${subdistrict}')`;
+        var query = `INSERT INTO order_books (user_id, order_id, book_name, quantity, total_price, book_id, shipID, district, province, postalcode, address, subdistrict) VALUES ( '${user_id}', '${order_id}', '${book_name}', '${quantity}','${total_price}','${book_id}', '${shipID}', '${district}', '${province}', '${postalcode}', '${address}', '${subdistrict}')`;
         dbConn.query(query, (err, rows) => {
             if (err) {
                 reject(err);
