29cc61f26dc3d37e6bdc538af76bd61aed0caaca
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-15 19:53:56 +0700

    release3

--- routes/cart.js
+++ routes/cart.js
@@ -157,7 +157,7 @@ router.post('/checkout', middleWare.isAuthenticatedCart, async (req, res, next) 
 
     var orderID = Math.round(Math.floor(Date.now() / 1000))
     //written by arit
-    var shipID = req.body ;
+    var shipID = req.body.shipIDtoSend ;
     console.log("Test shipID", shipID);
     var testNan = parseInt(shipID) ;
     console.log("Nan", testNan) ;
