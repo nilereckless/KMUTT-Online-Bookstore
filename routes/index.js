6695e348c48371b17cd42eec566d4778e98e77e6
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-16 16:25:46 +0700

    noti

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,11 @@ router.get('/', authentication.checkAdmin, async (req, res, next) => {
   console.log("test", req.user)
   var date = await viewbook.getDateNewestBook();
   var data = await viewbook.getNewestBook(moment(date).format('YYYY-MM-DD'), 3, 3);
+  var notifyUser = null ;
  // console.log(req.staff);
-   var notifyUser = await notificationController.getNotificationsByUserID(req.user.id) ;
+   if(req.user){
+     notifyUser = await notificationController.getNotificationsByUserID(req.user.id) ;
+   }
 
   res.render('index', { data: data, user: req.user, staff: req.staff, notify : notifyUser});
 })
