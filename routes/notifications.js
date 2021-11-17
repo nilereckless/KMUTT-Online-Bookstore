f004160e81922ffb5c8d3e1711d5c06315627c5e
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-17 17:10:38 +0700

    noti fix

--- routes/notifications.js
+++ routes/notifications.js
@@ -10,4 +10,13 @@ router.get('/',middleWare.isAuthenticatedCart, async (req, res, next) => {
    res.render('notification', {notify : notify}) ;
 })
 
+router.get('/getNoti/(:id)',middleWare.isAuthenticatedCart, async (req, res, next) => {
+   console.log("Params id" ,req.params.id) ;
+   console.log("Noti id", req.user.id) ;
+   console.log("What refreshclient", req.UserRefreshClient.id) ;
+   var noti = await NotificationController.getNotificationsByUserID(req.user.id) ; // req.user.id
+   //res.json(noti) ;
+   res.render('notification', {notify : notify}) ;
+})
+
 module.exports = router ;
