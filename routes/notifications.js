e39a1b2cc5d41a1c1395d0d6add4736ee96e0254
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-28 23:00:17 +0700

    FINISH

--- routes/notifications.js
+++ routes/notifications.js
@@ -20,8 +20,7 @@ router.get('/', middleWare.isAuthenticatedCart, authentication.checkAdmin, async
 
    for(var j = 0 ; j < count ; j++){
       console.log("Get total price from order ", orderNum) ;
-      console.log("ordernum 0 j total price", "or j 0 total price", orderNum[j][0].total_price) ;
-     total.push(orderNum[j].total_price) ;
+      
    }
 
    console.log("Total in noti ", total) ;
