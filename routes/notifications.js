a7e1324c6c83c82a42a2f957ba5ff9a114e1684b
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-28 22:21:44 +0700

    FINISH

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,13 +4,16 @@ let dbConn = require('../lib/db');
 var NotificationController = require('../controller/notificationController') ;
 const middleWare = require('../middleware/authentication');
 let authentication = require('../middleware/authentication');
+var orderBookController = require('../controller/orderHistoryController') ;
 
 router.get('/',middleWare.isAuthenticatedCart, authentication.checkAdmin,  async (req, res, next) => {
    var noti = await NotificationController.getNotificationsByUserID(req.user.id) ; // req.user.id
-   console.log("User id for noti", req.user.id) ;
-   console.log("Notification Backend Data " ,noti) ;
-   console.log("Noti display orderNumber ", noti["orderNumber"]) ;
-   console.log("Noti orderNum ", noti[0].orderNumber)
+   var total = 0 ;
+ //  console.log("User id for noti", req.user.id) ;
+   console.log("Noti orderNum ", noti[0].orderNumber) ;
+   var orderNum = orderBookController.getBookOrderByOrderID(noti[0].orderNumber) ;
+   console.log("Order in noti ", orderNum) ;
+   console.log("Total price in noti ", orderNum.total_price) ;
    //res.json(noti) ;
    res.render('notification', {notify : noti, user : req.user, staff: req.staff}) ;
 })
