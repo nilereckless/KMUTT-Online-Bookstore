3bcf604f3dbeb54a1212404b81e52a161331832f
nilereckless <56642226+nilereckless@users.noreply.github.com>
2021-11-28 23:11:02 +0700

    FINISH

--- routes/notifications.js
+++ routes/notifications.js
@@ -8,12 +8,14 @@ var orderBookController = require('../controller/orderHistoryController');
 
 router.get('/', middleWare.isAuthenticatedCart, authentication.checkAdmin, async (req, res, next) => {
    var noti = await NotificationController.getNotificationsByUserID(req.user.id); // req.user.id
+   var orderNum = [] ;
    var count = 0 ;
    var total = [];
 
    for(var i = 0 ; i < noti.length; i++){
       if(noti[i].status === "pending" || noti[i].status === "Pending"){
-         var orderNum = await orderBookController.getBookOrderByOrderID(noti[i].orderNumber);
+       //  var orderNum = await orderBookController.getBookOrderByOrderID(noti[i].orderNumber);
+       orderNum.push(noti[i].orderNumber) ;
          count+=1 ;
       }
    //   var orderNum = await orderBookController.getBookOrderByOrderID(noti[i].orderNumber);
@@ -22,12 +24,17 @@ router.get('/', middleWare.isAuthenticatedCart, authentication.checkAdmin, async
 
    console.log("count ", count) ;
 
-   for(var j = 0 ; j < count ; j++){
-      console.log("Get total price from order ", orderNum) ;
-      console.log("orerNum[j]", orderNum[j]) ;
+   for(var z = 0 ; z < count ; z++){
+      var testPrice = await orderBookController.getBookOrderByOrderID(count[z]);
+       console.log("Test price ", testPrice) ;
    }
 
-   console.log("Total in noti ", total) ;
+  /* for(var j = 0 ; j < count ; j++){
+      console.log("Get total price from order ", orderNum) ;
+      console.log("orderNum[j]", orderNum[j].total_price) ;
+   } */
+
+ //  console.log("Total in noti ", total) ;
  
    res.render('notification', { notify: noti, user: req.user, staff: req.staff, sumPrice: total });
 })
